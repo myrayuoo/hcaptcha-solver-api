26dc74cd5156aaca371b6e58a9b2f1c466004547
exploit <60840691+exploitxd@users.noreply.github.com>
2023-01-30 02:14:10 +0530

    example api usage

--- debug.py
+++ debug.py
@@ -4,13 +4,13 @@ os.system("cls")
 
 site_key = "f5561ba9-8f1e-40ca-9b5b-a0b3f719ef34"
 site_url = "https://discord.com"
-
 solver_api = "https://hcaptcha-solver.notexploit.tech/solve"
+_authorization = "Exploit321$"
 
 def get_solved_captcha(site_key, site_url):
     print("[+] Solving captcha for %s" % site_url)
     api = solver_api + "?site_key=%s&site_url=%s" % (site_key, site_url)
-    r = requests.get(api, headers={"Authorization": "Exploit321$"})
+    r = requests.get(api, headers={"Authorization": _authorization})
     print(r.text)
     print("\n\n")
     print("[+] Captcha Solved")
